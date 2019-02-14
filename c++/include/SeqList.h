41ac92e0612db5d53c6a65f1506052c611cf82bb
yanglin <185284033@qq.com>
2019-02-14 18:43:30 +0800

    update c++: 1. 增加 顺序存储线性表SeqList的实现

--- c++/include/SeqList.h
+++ c++/include/SeqList.h
@@ -20,17 +20,81 @@ protected:
     T *m_array;    //空间
     int m_length;  //大小
 public:
-    bool insert(int i, const T& e);
-    bool remove(int i); 
-    bool get(int i, const T& e) const;
-    bool set(int i, const T& e);
+    bool insert(int i, const T& e)
+    {
+        bool res = (i < m_length)&&(i >= 0);
+        res = res && ((m_length + 1) <= capacity());
+        if(res)
+        {
+            for(int p = m_length-1; p > i; p--)
+            {
+                m_array[p+1] = m_array[p];
+            }
+            m_array[i] = e;
+            m_length++;
+        }
+        return res;        
+    }
 
-    int length() const;
-    void clear();
+    bool remove(int i)
+    {
+        bool res = (i < m_length)&&(i >= 0);
+        if(res)
+        {
+            for(int p=i; p<m_length-1; p++)
+            {
+                m_array[p] = m_array[p+1];
+            }
+            m_length--;
+        }
+
+        return res;
+    }
+
+    bool get(int i, const T& e) const
+    {
+        bool res = (i<m_length)&&(i>=0);
+        //e = *(m_array + sizeof(T)*i);
+        if(res)
+            e = m_array[i];
+        return res;
+    }
+    bool set(int i, const T& e)
+    {
+        bool res = (i<m_length)&&(i>=0);
+        //memcpy(m_array + sizeof(T)*i, &e, sizeof(T));
+        if(res)
+            m_array[i] = e;
+
+        return res;       
+    }
+
+    int length() const
+    {
+        return m_length;
+    }
+    void clear()
+    {
+        m_length = 0;
+    }
 
     // 顺序存储线性表 提供数组的访问方式
-    T& operator[] (int i);
-    T  operator[] (int i) const;  //const数组
+    T& operator[] (int i)
+    {
+        if((i<m_length)&&(i>=0))
+        {
+            return m_array[i];
+        }
+        else
+        {
+            THROW_EXCEPTION(IndexOutOfBoundsException, "Index out of bound.");
+        }
+    }
+
+    T operator[] (int i) const //const数组
+    {
+       return (const_cast<SeqList& >(*this))[i]; 
+    }
 
     //空间容量
     virtual int capacity() const = 0; //子类设置空间和大小，所以容量函数也应由子类实现
