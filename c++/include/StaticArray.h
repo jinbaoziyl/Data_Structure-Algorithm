9d088ddda105cfd29591c980bd266ee4df0ea268
yanglin <185284033@qq.com>
2019-02-15 16:20:32 +0800

    update c++: 1.增加 固定内存的数组类StaticArray实现

---
+++ c++/include/StaticArray.h
@@ -0,0 +1,57 @@
+#ifndef __STATIC_ARRAY_H__
+#define __STATIC_ARRAY_H__
+
+namespace YLinLib
+{
+ template <typename T, int N>
+ class StaticArray : public Array<T>
+ {
+protected:
+    T m_space[N];
+
+public:
+    StaticArray()
+    {
+        m_array = m_space;
+    }
+
+    StaticArray(const StaticArray<T, N>& obj)
+    {
+        if(N == obj.length())
+        {
+            for(int i=0; i<length(); i++)
+            {
+                m_space[i] = obj.m_space[i];
+            }
+        }
+        else
+        {
+            cout << "No Valid Array to copy..." << endl;
+        } 
+    }
+
+    StaticArray<T, N>& operator= (const StaticArray<T, N>& obj)
+    {
+        if(N == obj.length())
+        {
+            if(this != &obj)
+            {
+                for(int i=0; i<length(); i++)
+                {
+                    m_space[i] = obj.m_space[i];
+                }        
+            }
+        }
+
+        return *this;
+    }
+
+    int length() const
+    {
+        return N;
+    }
+ };
+
+} // YLinLib
+
+#endif //__STATIC_ARRAY_H__
