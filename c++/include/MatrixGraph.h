b704229d34322fa061dba89d6a67cd85b138c5e5
jinbaoziyl <185284033@qq.com>
2019-04-10 22:06:02 +0800

    1. 增加 图的邻接矩阵中公有函数的操作

--- c++/include/MatrixGraph.h
+++ c++/include/MatrixGraph.h
@@ -2,7 +2,7 @@
 #define __MATRIXGRAPH_H__
 #include "Graph.h"
 #include "Exception.h"
-
+#include "DynamicArray_Old.h"
 namespace YLinLib
 {
 /*
@@ -92,15 +92,167 @@ public:
 
         return ret;       
     }
-    virtual SharePointer< Array<int> > getAdjacent(int i) = 0;
-    virtual E getEdge(int i, int j) = 0;
-    virtual bool getEdge(int i, int j, E& value) = 0;
-    virtual bool setEdge(int i, int j, const E& value) = 0;
-    virtual bool removeEdge(int i, int j) = 0;
-    virtual int vCount() = 0;
-    virtual int eCount() = 0;
-    virtual int oD(int i) = 0;
-    virtual int ID(int i) = 0;   
+    SharePointer< Array<int> > getAdjacent(int i) //获取与i相临接的其他顶点
+    {
+        DynamicArray<int>* ret = NULL;
+
+        if((i >= 0) && (i < vCount()))
+        {
+            int n = 0;
+            for(int j=0 ;j<vCount(); j++)
+            {
+                if(m_edges[i][j] != NULL)
+                {
+                    n++;
+                }
+            }
+
+            ret = new DynamicArray(n);
+            if(ret != NULL)
+            {
+                for(int j=0, k = 0;j<vCount(); j++)
+                {
+                    if(m_edges[i][j] != NULL)
+                    {
+                        ret->set(k++, j);
+                    }
+                }
+            }
+            else
+            {
+                THROW_EXCEPTION(NoEnoughMemoryException, "No Memory to alloc...");
+            }
+        }
+        else
+        {
+           THROW_EXCEPTION(InvalidParameterException,"Index i is invalid..."); 
+        }
+    }
+    E getEdge(int i, int j)  //获取i到j顶点的权值
+    {
+        E ret;
+        if(!getEdge(i, j, &ret))
+        {
+            THROW_EXCEPTION(InvalidParameterException,"Index i&j is invalid...");
+        }
+        return ret;
+    }
+    bool getEdge(int i, int j, E& value)
+    {
+        bool ret = ((i>=0) && (i<vCount())
+                    (j>=0) && (j<vCount()));
+        if(ret)
+        {
+            if(m_vertexes[i][j] ! = NULL)
+            {
+                value = *(m_vertexes[i][j]);
+            }
+            else
+            {
+                THROW_EXCEPTION(InvalidParameterException, "No value assigned to this vertex...");
+            }
+        }
+
+        return ret;        
+    }
+    bool setEdge(int i, int j, const E& value)
+    {
+        bool ret = ((i>=0) && (i<vCount())
+            (j>=0) && (j<vCount()));
+
+        if(ret)
+        {
+            E *ne = m_edges[i][j];
+            if(be == NULL)
+            {
+                ne = new E();
+                if(ne != NULL)
+                {
+                    *ne = value;
+                    m_edges[i][j] = ne;
+                    m_eCount++;
+                }
+                else
+                {
+                    THROW_EXCEPTION(NoEnoughMemoryException, "No Memory to alloc...");
+                }
+            }
+            else
+            {
+                *ne = value;
+            }
+        }
+    }
+    bool removeEdge(int i, int j)
+    {
+        bool ret = ((i>=0) && (i<vCount())
+            (j>=0) && (j<vCount()));
+        if(ret)
+        {
+            E* toDel = m_edges[i][j];
+            m_edges[i][j] = NULL;
+
+            if(toDel != NULL)
+            {
+                m_eCount--;
+
+                delete toDel;
+            }
+        }   
+
+        return ret;     
+    }
+    int vCount()
+    {
+        return N;
+    }
+    int eCount()  //获取图中边的数量
+    {
+        return m_eCount;
+    }
+    int oD(int i)  //获取 "出度"
+    {
+        int ret = 0;
+        if((i>=0) && (i<vCount()))
+        {
+            for(int i=0; i<vCount(); i++)
+            {
+                for(int j=0; j<vCount(); j++)
+                {
+                    if(m_edges[i][j] != NULL) //看i行中不为空的次数
+                    {
+                        ret++;
+                    }
+                }
+            }
+        }
+        else
+        {
+           THROW_EXCEPTION(InvalidParameterException, "Index i is invalid..."); 
+        }
+    }
+
+    int ID(int i)  //获取 "入度"
+    {
+         int ret = 0;
+        if((i>=0) && (i<vCount()))
+        {
+            for(int i=0; i<vCount(); i++)
+            {
+                for(int j=0; j<vCount(); j++)
+                {
+                    if(m_edges[j][i] != NULL)  //看i列中不为空的次数
+                    {
+                        ret++;
+                    }
+                }
+            }
+        }
+        else
+        {
+           THROW_EXCEPTION(InvalidParameterException, "Index i is invalid..."); 
+        }       
+    }  
 };
 }
 
