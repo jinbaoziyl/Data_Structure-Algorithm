50c88afd29dcf856148d503226794bf67d0c6824
jinbaoziyl <185284033@qq.com>
2019-04-11 23:46:01 +0800

    1.增加 图的遍历，广度优先算法(BFS): 以二叉树层序遍历的思想实现图的遍历 说明: 1) 将起始顶点压入队列 2) 弹出队头顶点v, 判断是否已经标记(标记->转2； 未标记->转3) 3) 标记顶点v，并将顶点v的邻接顶点压入队列 4) 判断队列是否为空(非空->转2 , 空: 结束)

--- c++/include/Graph.h
+++ c++/include/Graph.h
@@ -44,6 +44,23 @@ struct Edge: public Object
 template <typename V, typename E>
 class Graph : public Object
 {
+protected:
+    template<typename T>
+    DynamicArray<T> *toArray(LinkQueue<T>& queue)  //将队列转化成数组
+    {
+        DynamicArray<T> *ret = new DynamicArray<T>(queue.lenght());
+        if(ret != NULL)
+        {
+            for(int i=0; i<queue.length(); i++)
+            {
+                ret->set(i, queue.front());
+            }
+        }
+        else
+        {
+            THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create new DynamicArray...");
+        }
+    }
 public:
     virtual V getVertex(int i) = 0;
     virtual bool getVertex(int i, V& value) = 0;
@@ -60,6 +77,49 @@ public:
     virtual int TD(int i)
     {
         return OD(i) + ID(i);
+    }
+    /*
+    *  以二叉树层序遍历的思想对图进行遍历
+    * @i: 起始顶点编号
+    */
+    SharePointer< Array<int> > BFS(int i)
+    {
+        DynamicArray<int>* ret = NULL;
+
+        if((i>=0) && (i<vCount()))
+        {
+            LinkQueue<int> q;
+            LinkQueue<int> r;
+            DynamicArray<bool> visited(vCount());
+            for(int i=0; i<vCount(); i++)
+            {
+                visited[i] = false;
+            }
+
+            q.add(i); //先把第一个顶点放进队列
+            while(q.length() > 0)
+            {
+                int v = q.front();
+                q.remove();
+                if(!visited[v])
+                {
+                    SharePointer<Array<int> >aj = getAdjacent(v);
+                    for(int j=0; j<aj->length();j++)
+                    {
+                        q.add((*aj)[j]);
+                    }
+
+                    r.add(v);
+                    visited[v] = true;
+                }
+            }
+
+            ret = toArray(r);
+        }
+        else
+        {
+            THROW_EXCEPTION(InvalidParameterException, "Index i is invalid...");
+        }
     }
 };
 
