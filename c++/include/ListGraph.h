69187e6be6ce9c5eaf84f90923ef50a4700c207e
jinbaoziyl <185284033@qq.com>
2019-04-11 23:19:54 +0800

    1.增加 图的邻接链表的公有函数实现

--- c++/include/ListGraph.h
+++ c++/include/ListGraph.h
@@ -137,6 +137,158 @@ public:
            THROW_EXCEPTION(InvalidParameterException, "No vertex to delete...");  
         }
     }
+    SharedPointer< Array<int> >getAdjacent(int i)
+    {
+        DynamicArray<int> *ret = NULL;
+        if((i>=0) && (i<vCount()))
+        {
+            Vertex* vertex = m_list.get(i);
+
+            ret = new DynamicArray<int>(vertex->edge.length());
+            if(ret != NULL)
+            {
+                int k = 0;
+                for(vertex->edge.move(0); !vertex->edge.end(); vertex->edge.next(), k++)
+                {
+                    ret->set(k, vertex->edge.current().e); //遍历每条边，取出目的顶点
+                }
+            }
+            else
+            {
+                THROW_EXCEPTION(NoEnoughMemoryException, "No memory to create new Vertex value...");
+            }
+        }
+        else
+        {
+            THROW_EXCEPTION(InvalidParameterException, "Index i is invalid...");
+        }
+    }
+
+    E getEdge(int i, int j)
+    {
+        E ret;
+        if(!getEdge(i, j, ret))
+        {
+            THROW_EXCEPTION(InvalidParameterException, "No value assigned to this position....");
+        }
+        return ret;
+    }
+
+    bool getEdge(int i, int j, E& value)
+    {
+        int ret = ((i>=0) && (i<vCount()) && (j>=0) && (j<vCount()));
+
+        if(ret)
+        {
+           Vertex* vertex = m_list.get(i);
+           int pos = vertex->edge.find(Edge<E>(i,j));
+
+           if(pos > 0)
+           {
+               value = vertex->edge.get(pos).date;
+           } 
+           else
+           {
+               THROW_EXCEPTION(InvalidParameterException, "No value assigned to this position....");
+           }
+        }
+
+        return ret;
+    }
+
+    bool  setEdge(int i, int j, const E& value)
+    {
+        int ret = ((i>=0) && (i<vCount()) && (j>=0) && (j<vCount()));
+        if(ret)
+        {
+            Vertex* vertex = m_list.get(i);
+            int pos = vertex->edge.find(Edge<E>(i,j));
+            if(pos >= 0)
+            {
+                ret = vertex->edge.set(pos, Edge<E>(i,j,value));
+            }
+            else
+            {
+                ret = vertex->edge.set(0, Edge<E>(i,j,value));
+            }
+        }
+        
+        return ret;
+    }
+
+    bool removeEdge(int i, int j)
+    {
+        int ret = ((i>=0) && (i<vCount()) && (j>=0) && (j<vCount()));
+        if(ret)
+        {
+            Vertex* vertex = m_list.get(i);
+            int pos = vertex->edge.find(Edge<E>(i,j));
+            if(pos >= 0)
+            {
+                ret = vertex->edge.remove(pos);
+            }
+        }
+        
+        return ret;
+    }   
+
+    int vCount()
+    {
+        return m_list.length();
+    } 
+
+    int eCount()
+    {
+        int ret = 0;
+        for((i>=0) && (i<vCount()))
+        {
+            for(m_list.move(0); !m_list.end();m_list.next())
+            {
+                LinkList< Edge<E> >&edge = m_list.current()->edge;
+                for(edge.move(0); !edge.end(); edge.next())
+                {
+                    if(edeg.current().e == i)
+                    {
+                        ret++;
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            THROW_EXCEPTION(InvalidParameterException, "Index i is invalid...");
+        }
+
+        return ret;
+    }
+
+    int oD(int i)
+    {
+        int ret = 0;
+        if((i>=0) && (i<vCount()))
+        {
+            ret = m_list.get(i)->edge.length();
+        }
+        else
+        {
+            THROW_EXCEPTION(InvalidParameterException, "Index i is invalid...");
+        }
+
+        return ret;
+    }
+
+    ~ListGraph()
+    {
+        while(m_list.length() > 0)
+        {
+            Vertex *toDel = m_list.get(0);
+            m_list.remove(0);
+
+            delete toDel->data;
+            delete toDel;
+        }
+    }
 };
 }
 
