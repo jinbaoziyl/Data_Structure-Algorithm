85b9468f7f885c6b1a8f5f7299ee400e14d2d908
jinbaoziyl <185284033@qq.com>
2020-03-20 12:08:36 +0800

    1.增加 二分搜索变型题 2.增加 贪婪算法的专题总结

--- python/binarysearch.py
+++ python/binarysearch.py
@@ -64,7 +64,48 @@ def binarysearch_upperbound(nums, target, lo, hi):
     else:
         return binarysearch(nums,target,lo,mid-1)
 
-nums = [5,7,8,8,8,10]
-lo = binarysearch_lowerbound(nums, 8, 0, len(nums)-1)
-hi = binarysearch_upperbound(nums, 8, 0, len(nums)-1)
-print("lo: %d, hi: %d" %(lo, hi))
+# nums = [5,7,8,8,8,10]
+# lo = binarysearch_lowerbound(nums, 8, 0, len(nums)-1)
+# hi = binarysearch_upperbound(nums, 8, 0, len(nums)-1)
+# print("lo: %d, hi: %d" %(lo, hi))
+
+# 找模糊的边界
+def firstGreaterThen(nums, target, lo, hi):
+    if lo > hi:
+        return -1
+    
+    mid = lo + (hi - lo) // 2
+    if nums[mid] > target and (mid == 0 or nums[mid -1] <= target):
+        return mid
+    if nums[mid] <= target:   #同时需要注意nums[mid]=target,又是需要找大于target的数，从右半边找
+        return firstGreaterThen(nums,target,mid+1,hi)
+    else:
+        return firstGreaterThen(nums,target,lo,mid-1)
+
+# nums = [-2,0,1,4,7,9,10]
+# val = firstGreaterThen(nums,6,0, len(nums)-1)
+# print("val: %d" %val)
+
+# 二分搜索旋转过的排序数组:
+def binarysearch_arr(nums, target, lo, hi):
+    if lo > hi:
+        return -1
+    
+    mid = lo + (hi - lo) // 2
+    if nums[mid] == target:
+        return mid
+
+    if nums[lo] <= nums[mid]:
+        if nums[mid] > target and nums[lo] <= target:
+            return binarysearch_arr(nums, target, lo, mid-1)
+        else:
+            return binarysearch_arr(nums, target, mid+1, hi)
+    else:
+        if nums[mid] < target and nums[hi] >= target:
+            return binarysearch_arr(nums, target, mid+1, hi)
+        else:
+            return binarysearch_arr(nums, target, lo, mid-1)
+
+nums = [4,5,6,7,0,1,2]
+val = binarysearch_arr(nums, 0, 0, len(nums)-1)
+print("val: %d" %val)
