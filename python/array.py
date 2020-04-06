8aa32f9a6a493866d68870834f2bebd27145f49b
jinbaoziyl <185284033@qq.com>
2020-04-06 10:14:07 +0800

    1.增加 数组、字符串一些练习

---
+++ python/array.py
@@ -0,0 +1,92 @@
+#翻转字符串
+def reverse_str(str):
+    lo, hi = 0, len(str)-1
+    while lo <= hi:
+        str[lo], str[hi] = str[hi], str[lo]
+        lo += 1
+        hi -= 1
+
+# str = list(input())
+# reverse_str(str)
+# print("".join(str))
+
+#字符个数统计
+def char_count(vstr):
+    lut = [0]*256
+    for i in range(len(vstr)):
+        lut[ord(vstr[i])] += 1
+    count = 0
+    for j in range(len(lut)):
+        if lut[j] > 0:
+            count += 1
+    return count
+
+# str = list(input())
+# res = char_count(str)
+# print(res)
+
+# 字符串合并处理
+import sys
+def encode_str(input_str):
+    result = ''
+    enc_str = '1234567890abcdefABCDEF'
+    for i in input_str:
+        if i in enc_str:
+            int_i = int('0x' + i, 16)
+            revers_b = '{:04b}'.format(int_i)[::-1]
+            chr_i = hex(int(revers_b, 2)).replace('0x', '')
+            if chr_i.isalpha():
+                result += chr_i.upper()
+            else:
+                result += chr_i
+        else:
+            result += i
+    return result
+
+def ProcessString(str1, str2):
+    str_merge = str1 + str2
+    tmp_str1 = []
+    tmp_str2 = []
+    for i in range(1, len(str_merge)+1):
+        if i%2==0:
+            tmp_str2.append(str_merge[i-1])
+        else:
+            tmp_str1.append(str_merge[i-1])
+    #排序
+    tmp_str1.sort()
+    tmp_str2.sort()
+
+    # vstr = "".join(tmp_str1) + "".join(tmp_str2)
+    vstr = tmp_str1 + tmp_str2
+    return encode_str(vstr)
+
+# str1, str2 = sys.stdin.readline().strip().split()
+# str_res = ProcessString(str1, str2)
+# print(str_res)
+
+#数字颠倒
+def reverse_num(a):
+    vlist = []
+
+    while a:
+        vlist.append(a%10)
+        a = a // 10
+    ss = list(map(str, vlist))
+    print("".join(ss))
+
+# vstr = input()
+# reverse_num(int(vstr))
+
+# 数字转变成二进制1的个数
+def bit_num(a):
+    count = 0
+    while a != 1:
+        b = a % 2
+        if b == 1:
+            count += 1
+        a = a // 2
+    
+    return count+1
+
+vstr = input()
+print(bit_num(int(vstr)))
